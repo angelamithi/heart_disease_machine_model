9f69c605f856b49d9039af61101c801e3a545227
+ <+>
2024-03-22 11:28:10 +0300

    ammend prediction code

--- app/routes/model.py
+++ app/routes/model.py
@@ -1,30 +1,65 @@
 import pickle
 import numpy as np
-from flask import Blueprint,make_response, jsonify
-from flask_restful import Api, Resource, abort, reqparse
+from flask import Blueprint, make_response, jsonify
+from flask_restful import Api, Resource, reqparse
 
-
-# create classification model  blueprint
+# create classification model blueprint
 Classification_bp = Blueprint('classification_bp', __name__)
 
-# register blueprints with  api
-
+# register blueprints with api
 api = Api(Classification_bp)
 
 # Load the classification model
-model=pickle.load(open("gs_log_model_1.pkl","rb"))
-post_args = reqparse.RequestParser()
-post_args.add_argument('name', type=str, required=True,
-                       help="name is required")
+model = pickle.load(open("gs_log_model_1.pkl", "rb"))
 
+post_args = reqparse.RequestParser()
+post_args.add_argument('age', type=float, required=True, help="age is required")
+post_args.add_argument('sex', type=str, required=True, help="sex is required (male or female)")
+post_args.add_argument('cp', type=float, required=True, help="cp is required")
+post_args.add_argument('trestbps', type=float, required=True, help="trestbps is required")
+post_args.add_argument('chol', type=float, required=True, help="chol is required")
+post_args.add_argument('fbs', type=float, required=True, help="fbs is required (fasting blood sugar in mg/dl)")
+post_args.add_argument('restecg', type=float, required=True, help="restecg is required")
+post_args.add_argument('thalach', type=float, required=True, help="thalach is required")
+post_args.add_argument('exang', type=str, required=True, help="exang is required (yes or no)")
+post_args.add_argument('oldpeak', type=float, required=True, help="oldpeak is required")
+post_args.add_argument('slope', type=float, required=True, help="slope is required")
+post_args.add_argument('ca', type=float, required=True, help="ca is required")
+post_args.add_argument('thal', type=float, required=True, help="thal is required")
 
 class ModelPrediction(Resource):
     def get(self):
-        #Find a way to grab all the values from the front end 
-
-        #convert all the features into a numpy array
-        features=[np.array(all_features)]
-        prediction=model.predict(features)
+        args = post_args.parse_args()
+        
+        # Mapping sex to 1 (male) or 0 (female)
+        sex = 1 if args['sex'].lower() == 'male' else 0
+        
+        # Mapping fbs > 120 mg/dl to 1 and fbs <= 120 mg/dl to 0
+        fbs = 1 if args['fbs'] > 120 else 0
+        
+        # Mapping exang to 1 (yes) or 0 (no)
+        exang = 1 if args['exang'].lower() == 'yes' else 0
+        
+        # Inserting variables into all_features array
+        all_features = [
+            args['age'], 
+            sex,
+            args['cp'],
+            args['trestbps'],
+            args['chol'],
+            fbs,
+            args['restecg'],
+            args['thalach'],
+            exang,
+            args['oldpeak'],
+            args['slope'],
+            args['ca'],
+            args['thal']
+        ]
+        
+        features = np.array([all_features])
+        prediction = model.predict(features)
         response = make_response(jsonify(prediction), 200)
         return response
-api.add_resource(ModelPrediction,"/predict")
+
+api.add_resource(ModelPrediction, "/predict")
